c660203764304312bb9941f2e1ed18589142bef0
Philipp Hanes <philip.hanes@gmail.com>
2010-11-14 18:21:12 -0500

    quadrant ratings

--- src/com/geohaven/android/CrimeHelper.java
+++ src/com/geohaven/android/CrimeHelper.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -85,5 +86,41 @@ public class CrimeHelper {
 
 	public static int countInRectangle(List<Crime> list, double latC, double longC, double width, double height) {
 		return getByRectangle(list, latC, longC, width, height).size();
+	}
+
+	public static int[] getQuadrantRatings(double latC, double longC, double width, double height) {
+		int[] results = new int[9];
+		Crime.Category cat = Crime.Category.WALKING;
+
+		results[0] = countInRectangle(cat, latC + height / 3, longC - width / 3, width / 3, height / 3);
+		results[1] = countInRectangle(cat, latC + height / 3, longC, width / 3, height / 3);
+		results[2] = countInRectangle(cat, latC + height / 3, longC + width / 3, width / 3, height / 3);
+		results[3] = countInRectangle(cat, latC, longC - width / 3, width / 3, height / 3);
+		results[4] = countInRectangle(cat, latC, longC, width / 3, height / 3);
+		results[5] = countInRectangle(cat, latC, longC + width / 3, width / 3, height / 3);
+		results[6] = countInRectangle(cat, latC - height / 3, longC - width / 3, width / 3, height / 3);
+		results[7] = countInRectangle(cat, latC - height / 3, longC, width / 3, height / 3);
+		results[8] = countInRectangle(cat, latC - height / 3, longC + width / 3, width / 3, height / 3);
+
+		List<Integer> l = new ArrayList<Integer>();
+		for (int i : results) {
+			l.add(i);
+		}
+		Collections.sort(l);
+
+		double lowCutoff = (l.get(3) + l.get(2)) * 1.0 / 2;
+		double highCutoff = (l.get(6) + l.get(5)) * 1.0 / 2;
+
+		for (int i = 0; i < results.length; i++) {
+			if (results[i] < lowCutoff) {
+				results[i] = 1;
+			} else if (results[i] > highCutoff) {
+				results[i] = 3;
+			} else {
+				results[i] = 2;
+			}
+		}
+
+		return results;
 	}
 }
